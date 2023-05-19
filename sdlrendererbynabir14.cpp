a27b55ca0caabf930dee7f656df2ebcd630b7d11
Nabir14 <82253045+Nabir14@users.noreply.github.com>
2023-05-19 20:39:54 +0600

    Add files via upload

---
+++ sdlrendererbynabir14.cpp
@@ -0,0 +1,42 @@
+//made by Nabir14
+#include <iostream>
+#include <SDL2/SDL.h>
+using namespace std;
+int main()
+{
+	SDL_Window *window = nullptr;
+	SDL_Surface *windowSurface = nullptr;
+	SDL_Surface *imageSurface = nullptr;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "error" << endl;
+	}
+	else
+	{
+		window = SDL_CreateWindow("SDL_Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 400, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			cout << "error" << endl;
+		}
+		else
+		{
+			windowSurface = SDL_GetWindowSurface(window);
+			imageSurface = SDL_LoadBMP("test.bmp");
+			if (imageSurface == NULL)
+			{
+				cout << "error" << endl;
+			}
+			else
+			{
+				SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+				SDL_UpdateWindowSurface(window);
+				SDL_Delay(200000);
+			}
+		}
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+
+		return 0;
+	}
+}
